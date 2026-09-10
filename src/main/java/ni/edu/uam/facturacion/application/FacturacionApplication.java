816e694dc2a31c132571aa7cd0b06f7ded43ce67
BJavi2106 <bjrochav@uamv.edu.ni>
2026-09-10 16:23:59 -0600

    feat: completar menu principal y navegacion

--- src/main/java/ni/edu/uam/facturacion/application/FacturacionApplication.java
+++ src/main/java/ni/edu/uam/facturacion/application/FacturacionApplication.java
@@ -5,41 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.net.URL;
-
 public class FacturacionApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        URL recursoFXML = getClass().getResource(
-                "/ni/edu/uam/facturacion/fxml/menu-principal.fxml"
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource(
+                        "/ni/edu/uam/facturacion/fxml/menu-principal.fxml"
+                )
         );
 
-        if (recursoFXML == null) {
-            throw new RuntimeException(
-                    "No se encontró el archivo menu-principal.fxml. " +
-                            "Verifique que esté en: " +
-                            "src/main/resources/ni/edu/uam/facturacion/fxml/"
-            );
-        }
-
-        FXMLLoader loader = new FXMLLoader(recursoFXML);
-
-        Scene scene = new Scene(loader.load());
-
-        stage.setTitle("Sistema de Facturación - Distribuidora El Güegüense");
-
-        stage.setScene(scene);
-
-        stage.setWidth(1000);
-
-        stage.setHeight(650);
-
-        stage.setMinWidth(800);
-
-        stage.setMinHeight(500);
-
+        stage.setTitle("Sistema de facturación");
+        stage.setScene(new Scene(loader.load(), 900, 600));
         stage.show();
     }
 
